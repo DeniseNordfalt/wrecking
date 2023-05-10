9e9c9e062d8115874ec5ff70080b5b67ff940c9b
Denise Nordfalt <denisenordfalt@gmail.com>
2023-05-11 00:18:22 +0200

    css static support

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import Team from "./models/team.js";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
-// import router from "./routes";
+//import router from "./routes";
 import { setupMongoDb } from "./config/db.js";
 import { seed } from "./config/seed.js";
 
 import GameRound from "./gameround.js";
 import { playGameRound, watchGameRound } from "./gameround_two.js";
 
-import routes from "./routes/index.js";
+import router from "./routes/index.js";
 
 const app = express();
 const PORT = 3000;
@@ -23,29 +24,26 @@ app.use(json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   res.send("server is running");
-// });
+//makes sure the server can handle css stylesheets
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-//app.use("/", routes);
-app.get("/", async (req, res) => {
-  //get all teams
-  const teams = await Team.find();
-  res.send(teams);
-});
+app.use(express.static(join(__dirname, "public")));
 
-//const MONGODB_URI = "mongodb://127.0.0.1/wrecking";
+app.use("/", router);
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb://127.0.0.1/wrecking";
+
+//const MONGODB_URI = process.env.MONGODB_URI;
 
 app.listen(PORT, () => {
   setupMongoDb(MONGODB_URI);
-  //seed();
+  seed();
   //const gameRound = new GameRound();
   //gameRound.start();
   //gameRound.watch();
   //playGameRound();
-  //watchGameRound();
+  watchGameRound();
 
   console.log(`Started Express at port ${PORT}`);
 });
