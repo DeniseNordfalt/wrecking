fa2f2aa281a6d5f33f860c02d73805dc34e36d5c
Denise Nordfalt <denisenordfalt@gmail.com>
2023-05-16 08:05:39 +0200

    moved seed to setup

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import { connect } from "mongoose";
+import { seed } from "./seed.js";
 
 export const setupMongoDb = async (url) => {
   try {
     await connect(url);
     console.log("Connected to MongoDB successfully");
+    await seed();
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
   }
