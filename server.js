def982195be80625579af153d78a56880215e5dc
Denise Nordfalt <denisenordfalt@gmail.com>
2023-05-16 08:05:26 +0200

    added method override

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import session from "express-session";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
 dotenv.config();
 
-//import router from "./routes";
 import { setupMongoDb } from "./config/db.js";
-import { seed } from "./config/seed.js";
 
 // import { playGameRound, watchGameRound } from "./gameround_two.js";
 import { watchGameRound } from "./gameround_two.js";
@@ -20,21 +20,35 @@ import router from "./routes/index.js";
 const app = express();
 const PORT = 3000;
 
+const oneDay = 1000 * 60 * 60 * 24;
+//const oneHour = 1000 * 60 * 60;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 60 * 60 * 1000,
+      maxAge: oneDay,
     },
   })
 );
 
 app.use(cors());
 app.use(json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  if (req.session.userId) {
+    res.locals.currentUser = req.session.userId;
+  }
+
+  next();
+});
 
 //makes sure the server can handle css stylesheets
 const __filename = fileURLToPath(import.meta.url);
@@ -49,7 +63,6 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/wrecking";
 
 app.listen(PORT, () => {
   setupMongoDb(MONGODB_URI);
-  seed();
   //playGameRound();
   watchGameRound();
 
