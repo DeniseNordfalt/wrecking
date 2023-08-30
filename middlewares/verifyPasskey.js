c59ea4c470d65015ce547242310a800edff21cc9
DeniseNordfalt <kaoskatt@gmail.com>
2023-08-30 22:55:19 +0200

    changed to accept body too

--- middlewares/verifyPasskey.js
+++ middlewares/verifyPasskey.js
@@ -1,8 +1,24 @@
 const verifyPasskey = async (req, res, next) => {
-  const inputString = req.query.key;
-  const pairs = inputString.split(";");
-  let key = pairs[0];
+  const inputString = req.query.key || null;
+  const key_two = req.body.data?.key || null;
+  let key = "";
 
+  console.log('inputString', inputString)
+
+  if(inputString !== undefined && inputString !== null){
+  const pairs = inputString.split(";");
+  key = pairs[0];
+  }
+  console.log('key', key)
+  console.log('key_two', key_two)
+  console.log('process.env.submit_password', process.env.submit_password)
+
+  
+  if(key_two !== undefined && key_two !== null){
+  key = key_two;
+  }
+
+  console.log('key', key)
   if (key === process.env.submit_password) {
     console.log("passkey verified");
     next();
